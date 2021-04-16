08b3cd1a228354bb7c527f4fa1a343a634c22871
Rahil Ali <rahil.eliyev@gmail.com>
2021-04-16 18:59:47 +0400

    responsive and hamburger button

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-window.onscroll = function () {
-  myFunction();
-};
-
-function myFunction() {
+window.addEventListener("scroll", () => {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 100) {
     document.querySelector("body nav").classList.add("active");
+    document.querySelector("body nav button").style.padding = "13.5px 46px";
   } else {
     document.querySelector("body nav").classList.remove("active");
+    document.querySelector("body nav button").style.padding = "30px 46px";
   }
-}
+});
